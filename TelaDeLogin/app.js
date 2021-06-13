53eb0cb16e0c7b6f07edcab1334270d18a084f2e
Samantha Abreu <71658031+SamanthaAbreu@users.noreply.github.com>
2021-06-13 14:30:16 -0300

    Update app.js

--- TelaDeLogin/app.js
+++ TelaDeLogin/app.js
@@ -1,22 +1,36 @@
-var btnSignin = document.querySelector("#signin");
-var btnSignup = document.querySelector("#signup");
+// var btnSignin = document.querySelector("#signin");
+// var btnSignup = document.querySelector("#signup");
 
-var body = document.querySelector("body");
-
-
-btnSignin.addEventListener("click", function () {
-   body.className = "sign-in-js"; 
-});
-
-btnSignup.addEventListener("click", function () {
-    body.className = "sign-up-js";
-})
+// var body = document.querySelector("body");
 
 
-var capturando = "",
-function capturar (){
-    capturando = document.getElementById('nome').value;
-    capturando = document.getElementById('email1').value;
+// btnSignin.addEventListener("click", function () {
+//    body.className = "sign-in-js"; 
+// });
+
+// btnSignup.addEventListener("click", function () {
+//     body.className = "sign-up-js";
+// })
+
+
+// var capturando = "",
+// function capturar (){
+//     capturando = document.getElementById('nome').value;
+//     capturando = document.getElementById('email1').value;
     
 
+// }
+
+
+// DECLAREI UMA VARIAVEL PARA ARMAZENAR O VALOR DO QUE EU VOU CAPTURAR COM COM O CLICK DO MOUSE
+var capNome = "";
+
+
+function captura(){
+    
+    // ESSE LINHA CAPTURA O VALOR DO INPUT NOME PELO (ID="NOME") LA NO CODIGO HTML 
+    capNome = document.getElementById('nome').value;
+
+    // ESSA LINHA MOSTRA UM ALERTA NA TELA 
+    alert("Bem-vindo(a) - "+capNome + "\n" + "Você receberá um e-mail com todas as novidades!" )
 }
